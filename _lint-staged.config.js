f5fc285773aa660ff29457e97f1c278b0f05eb78
louis <louis@lemoncloud.io>
2024-03-26 13:39:16 +0900

    chore: temp commit

--- _lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    '{apps,tools,libs}/**/*.{ts,js,tsx,jsx,json,md,html,css,scss}': [
-        'nx affected --target lint --uncommitted --fix true',
-        'nx format:write --uncommitted',
-    ],
-};
